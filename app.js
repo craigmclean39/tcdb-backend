233d1b9746c5c6da5619273e4b8d8f01f23091af
Craig McLean <craig.mclean39@gmail.com>
2022-02-03 16:29:22 -0800

    Connect to mongodb

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const mongoose = require('mongoose');
+const login = process.env.MONGODB_LOGIN;
+const mongoDB = `mongodb+srv://${login}@sandbox.1gheh.mongodb.net/truecrimedb?retryWrites=true&w=majority`;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var app = express();
 
