2c50764205a96425e8067284d417f5ea9dd5a945
Craig McLean <craig.mclean39@gmail.com>
2022-02-03 17:40:42 -0800

    Added cors middleware
    Currently accepting all requests

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -17,6 +18,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
